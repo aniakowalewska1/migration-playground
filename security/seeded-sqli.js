2e2aac6b33ec345939b0dc911fea7a2a731c5225
Anna Kowalewska <anna.kowalewska@basic-fit.com>
2025-10-16 14:08:00 +0200

    fix: simplify query construction in /user endpoint

--- security/seeded-sqli.js
+++ security/seeded-sqli.js
@@ -1,5 +1,4 @@
 app.get("/user", (req, res) => {
-  const id = req.query.id;
-  const query = `SELECT * FROM users WHERE id = ${id}`;
+  const query = `SELECT * FROM users WHERE id = ${req.query.id}`;
   db.query(query, (err, rows) => res.json(rows));
 });
