973937b6de5c057e8aff4535efd04099b6cf40ff
Anna Kowalewska <anna.kowalewska@basic-fit.com>
2025-11-17 09:27:17 +0100

    refactor: remove vulnerability comment from TestPage component

--- app/pages/test-xss.tsx
+++ app/pages/test-xss.tsx
@@ -3,7 +3,6 @@ import XssSeed from "../../security/xss-sink";
 interface TestPageProps {
   userInput: string;
 }
-// Vulnerability
 
 export default function TestPage({ userInput }: TestPageProps) {
   return <XssSeed userContent={userInput} />;
